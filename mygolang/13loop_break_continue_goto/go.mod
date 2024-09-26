ba5e6bdd0abe73b5a0e949f001c1b839f874c7b7
Vedantjn <vedant.jain.indore@gmail.com>
2024-09-26 17:55:42 +0530

    loop break continue goto

---
+++ mygolang/13loop_break_continue_goto/go.mod
@@ -0,0 +1,3 @@
+module loop_break_continue_goto
+
+go 1.23.1
---
+++ mygolang/13loop_break_continue_goto/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to Loops in Golang")
+
+	days := []string{"Sunday", "Tuesday", "Wednesday", "Friday", "Saturday"}
+
+	fmt.Println(days)
+
+	// Type 1
+	for d := 0; d < len(days); d++ {
+		fmt.Println(days[d])
+	}
+
+	// Type 2
+	for i := range days {
+		fmt.Println(days[i])
+	}
+
+	// Type 3
+	for index, day := range days {
+		fmt.Printf("index is %v and value is %v\n", index, day)
+	}
+
+	// goto statement
+	rogueValue := 1
+	for rogueValue < 10 {
+
+		if rogueValue == 2 {
+			goto lco
+		}
+
+		if rogueValue == 5 {
+			rogueValue++
+			continue
+		}
+
+		fmt.Println("Value is: ", rogueValue)
+		rogueValue++
+	}
+
+lco:
+	fmt.Println("Jumping at vedantjain.vercel.app")
+}
