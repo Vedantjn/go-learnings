3c264eeb7796707e70e4cac49f448ada570b9126
Vedantjn <vedant.jain.indore@gmail.com>
2024-09-27 15:26:29 +0530

    handling and testing routes

--- mygolang/27building_api_models/main.go
+++ mygolang/27building_api_models/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -34,8 +35,23 @@ func (c *Course) IsEmpty() bool {
 }
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("API - LearnCodeOnline.in")
+	r := mux.NewRouter()
 
+	// seeding
+	courses = append(courses, Course{CourseId: "2", CourseName: "ReactJS", CoursePrice: 299, Author: &Author{FullName: "Vedant Jain", Website: "lco.dev"}})
+	courses = append(courses, Course{CourseId: "4", CourseName: "MERN Stack", CoursePrice: 199, Author: &Author{FullName: "Vedant Jain", Website: "go.dev"}})
+
+	// routing
+	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/courses", getAllCourses).Methods("GET")
+	r.HandleFunc("/course/{id}", getOneCourse).Methods("GET")
+	r.HandleFunc("/course", createOneCourse).Methods("POST")
+	r.HandleFunc("/course/{id}", updateOneCourse).Methods("PUT")
+	r.HandleFunc("/course/{id}", deleteOneCourse).Methods("DELETE")
+
+	// listen to a port
+	log.Fatal(http.ListenAndServe(":4000", r))
 }
 
 // controllers
@@ -86,6 +102,16 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// TODO: check only if title is duplicate
+	// loop, title matches with course.coursename, JSON
+	// if it does, send an error
+	for _, existingCourse := range courses {
+		if existingCourse.CourseName == course.CourseName {
+			json.NewEncoder(w).Encode("Course with this title already exists")
+			return
+		}
+	}
+
 	// generate unique id, string
 	// append course into courses
 	rand.Seed(time.Now().UnixNano())
@@ -126,6 +152,8 @@ func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
 	for index, course := range courses {
 		if course.CourseId == params["id"] {
 			courses = append(courses[:index], courses[index+1:]...)
+			// TODO: send a confirm or deny response
+			json.NewEncoder(w).Encode("Course deleted successfully")
 			break
 		}
 	}
