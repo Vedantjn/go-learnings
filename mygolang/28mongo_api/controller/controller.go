0aa70eec466af9c8964d519c895313e1481327ca
Vedantjn <vedant.jain.indore@gmail.com>
2024-09-27 17:18:14 +0530

    update a recored in mongodb

--- mygolang/28mongo_api/controller/controller.go
+++ mygolang/28mongo_api/controller/controller.go
@@ -6,6 +6,8 @@ import (
 	"log"
 
 	"github.com/Vedantjn/mongo_api/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -45,4 +47,16 @@ func insertOneMovie(movie model.Netflix) {
 		log.Fatal(err)
 	}
 	fmt.Println("Inserted 1 movie in db with id: ", inserted.InsertedID)
+}
+
+// update 1 record
+func updateOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Modified count: ", result.ModifiedCount)
 }
