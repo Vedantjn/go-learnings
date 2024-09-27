9b417680f0411a9cca1f8571d6379a9fc447b563
Vedantjn <vedant.jain.indore@gmail.com>
2024-09-27 17:28:45 +0530

    delete one and delete many in mongodb

--- mygolang/28mongo_api/controller/controller.go
+++ mygolang/28mongo_api/controller/controller.go
@@ -60,3 +60,24 @@ func updateOneMovie(movieId string) {
 	}
 	fmt.Println("Modified count: ", result.ModifiedCount)
 }
+
+// delete 1 record
+func deleteOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	deleteCount, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Movie got deleted with delete count: ", deleteCount)
+}
+
+// delete all records from mongoDB
+func deleteAllMovie() int64 {
+	deleteResult, err := collection.DeleteMany(context.Background(), bson.D{{}}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Number of movies deleted: ", deleteResult.DeletedCount)
+	return deleteResult.DeletedCount
+}
